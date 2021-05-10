500536e3a2818cf99440602f01826c1a781ae14e
AbishaZaigo <abishavimala1995@gmail.com>
2021-05-10 11:49:42 +0530

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+
+    <h1>Welcome</h1>
+    <a href="/about">ABOUT US</a><br>
+    <a href="/contact">CONTACT US</a>   
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    // return redirect('about');
+});
+
+// Passing data with routing
+
+// Route::get('/{name}', function ($name) {
+//     return view('welcome', ['name'=>$name]);
+// });
+
+Route :: view('about', 'about');
+
+Route::get('/contact', function () {
+    return view('contact');
+});
