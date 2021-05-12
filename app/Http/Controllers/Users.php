2f3ee2c6c14bfe526057c89eadf3ea65eed0607a
AbishaZaigo <abishavimala1995@gmail.com>
2021-05-12 10:39:05 +0530

    Form validation with regex

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -9,7 +9,7 @@ class Users extends Controller
     //
     function getData(Request $req){
         $req->validate([
-            'name'=>'required | max:10',
+            'name'=>'required | max:10 | regex:/^[a-z]/u',
             'password'=>'required | min:5'
         ]);
         return $req->input();
