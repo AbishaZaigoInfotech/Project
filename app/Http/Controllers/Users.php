8cc2592e946adb4958b648b9bc10aa89f8039200
AbishaZaigo <abishavimala1995@gmail.com>
2021-05-12 10:15:19 +0530

    Login form validation

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -8,6 +8,10 @@ class Users extends Controller
 {
     //
     function getData(Request $req){
+        $req->validate([
+            'name'=>'required | max:10',
+            'password'=>'required | min:5'
+        ]);
         return $req->input();
     }
 }
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,7 +1,11 @@
 <h1>Login form</h1>
 <form action="users" method="POST">
 @csrf
-<input type="text" name="name"><br><br>
-<input type="password" name="password"><br><br>
+<input type="text" name="name"><br>
+<span style="color:red">@error('name'){{$message}}@enderror</span>
+<br><br>
+<input type="password" name="password"><br>
+<span style="color:red">@error('password'){{$message}}@enderror</span>
+<br><br>
 <button type="submit">Login</button>
 </form>
